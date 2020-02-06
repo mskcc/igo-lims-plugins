0b3f8b279be81131e3c433d8987c6c3fc25b1f4e
as114 <ajaypaulsharma@yahoo.co.in>
2020-02-06 17:45:32 -0500

    made changes according to code review

--- src/main/java/com/velox/sloan/cmo/workflows/IgoLimsPluginUtils/Tags.java
+++ src/main/java/com/velox/sloan/cmo/workflows/IgoLimsPluginUtils/Tags.java
@@ -2,16 +2,16 @@ package com.velox.sloan.cmo.workflows.IgoLimsPluginUtils;
 
 public interface Tags {
 
-    final String STR_SAMPLESHEET_WELL_ID = ".*<!--\\s*STR\\s*SAMPLESHEET\\s*WELL\\s*ID-->.*";
+    String STR_SAMPLESHEET_WELL_ID = ".*<!--\\s*STR\\s*SAMPLESHEET\\s*WELL\\s*ID-->.*";
 
-    final String STR_SAMPLESHEET_SAMPLE_TYPE = ".*<!--\\s*STR\\s*SAMPLESHEET\\s*SAMPLE\\s*TYPE-->.*";
+    String STR_SAMPLESHEET_SAMPLE_TYPE = ".*<!--\\s*STR\\s*SAMPLESHEET\\s*SAMPLE\\s*TYPE-->.*";
 
-    final String STR_SAMPLESHEET_APPLICATION_TYPE = ".*<!--\\s*STR\\s*SAMPLESHEET\\s*APPLICATION\\s*TYPE-->.*";
+    String STR_SAMPLESHEET_APPLICATION_TYPE = ".*<!--\\s*STR\\s*SAMPLESHEET\\s*APPLICATION\\s*TYPE-->.*";
 
-    final String STR_SAMPLESHEET_SIZE_STANDARD = ".*<!--\\s*STR\\s*SAMPLESHEET\\s*SIZE\\s*STANDARD-->.*";
+    String STR_SAMPLESHEET_SIZE_STANDARD = ".*<!--\\s*STR\\s*SAMPLESHEET\\s*SIZE\\s*STANDARD-->.*";
 
-    final String STR_SAMPLESHEET_DYE_SET = ".*<!--\\s*STR\\s*SAMPLESHEET\\s*DYE\\s*SET-->.*";
+    String STR_SAMPLESHEET_DYE_SET = ".*<!--\\s*STR\\s*SAMPLESHEET\\s*DYE\\s*SET-->.*";
 
-    final String STR_SAMPLESHEET_RUN_MODULE = ".*<!--\\s*STR\\s*SAMPLESHEET\\s*RUN\\s*MODULE-->.*";
+    String STR_SAMPLESHEET_RUN_MODULE = ".*<!--\\s*STR\\s*SAMPLESHEET\\s*RUN\\s*MODULE-->.*";
 
 }
