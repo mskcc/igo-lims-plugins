e7fd437f57df515df9187f189b481daab2065cd4
David Streid <david.streid.nyc@gmail.com>
2020-07-29 09:06:19 -0400

    Fixing logging issue

--- src/main/java/com/velox/sloan/cmo/workflows/utils/DataAccessUtils.java
+++ src/main/java/com/velox/sloan/cmo/workflows/utils/DataAccessUtils.java
@@ -1,15 +1,11 @@
 package com.velox.sloan.cmo.workflows.utils;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
 import com.velox.api.datarecord.DataRecord;
 import com.velox.api.datarecord.NotFound;
 import com.velox.api.user.User;
 import java.rmi.RemoteException;
 
 public class DataAccessUtils {
-    private static final Logger LOGGER = LoggerFactory.getLogger(DataAccessUtils.class);
-
     /**
      * Safely retrieves a String value from a dataRecord. Returns empty string on error.
      *
@@ -18,14 +14,13 @@ public class DataAccessUtils {
      * @param user
      * @return
      */
-    public static String getRecordStringValue(DataRecord record, String key, User user) {
+    public static String getRecordStringValue(DataRecord record, String key, User user) throws IllegalStateException {
         try {
             if (record.getValue(key, user) != null) {
                 return record.getStringVal(key, user);
             }
         } catch (NotFound | RemoteException | NullPointerException e) {
-            LOGGER.error(String.format("Failed to get (String) key %s from Data Record: %d", key, record.getRecordId()));
-            return "";
+            throw new IllegalStateException(String.format("Failed to get (String) key %s from Data Record: %d", key, record.getRecordId()));
         }
         return "";
     }
