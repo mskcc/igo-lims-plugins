da58b87b9b4ebb3823cbc36e9c2f63972df45bca
Ajay Sharma <ajaypaulsharma@yahoo.co.in>
2021-04-15 16:15:09 -0400

    updated plugin logic to - do not prompt user if only one runtype option for a recipe in reference table - do not prompt user if only one or no Panel Name option for a recipe in reference table - do not run plugin in Sample type is 'Pooled Library'.

--- src/main/java/com/velox/sloan/cmo/workflows/samplereceiving/sequencingrequirements/CoverageToReadsConverter.java
+++ src/main/java/com/velox/sloan/cmo/workflows/samplereceiving/sequencingrequirements/CoverageToReadsConverter.java
@@ -8,21 +8,28 @@ import com.velox.api.plugin.PluginResult;
 import com.velox.api.util.ServerException;
 import com.velox.sapioutils.server.plugin.DefaultGenericPlugin;
 import com.velox.sapioutils.shared.enums.PluginOrder;
+import com.velox.sloan.cmo.recmodels.SampleModel;
 import com.velox.sloan.cmo.workflows.IgoLimsPluginUtils.IgoLimsPluginUtils;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 
+/**
+ * Plugin to populate Requested Reads for samples that require Coverage
+ * using the 'Recipe, CapturePanel, RunType, TumorOrNormal, Species, Coverage' combinations defined in
+ * 'ApplicationReadCoverageRef' table in LIMS. The plugin parses data from Samples and Banked Samples
+ * to find values for above mentioned fields. If a match is found, the plugin will update the Sequencing Requirements
+ * or Will display an error message.
+ *
+ * @author sharmaa1
+ */
 public class CoverageToReadsConverter extends DefaultGenericPlugin {
     IgoLimsPluginUtils util = new IgoLimsPluginUtils();
     private boolean panelSelectionOffered = false;
     private Object runType = null;
     private Object panelName = null;
+    private Object recipe = null;
 
     public CoverageToReadsConverter() {
         this.setTaskEntry(true);
@@ -49,6 +56,27 @@ public class CoverageToReadsConverter extends DefaultGenericPlugin {
                 this.clientCallback.displayError("Samples not attached to this task.");
                 return new PluginResult(false);
             }
+            recipe = attachedSamples.get(0).getValue(SampleModel.RECIPE, user);
+            if (Objects.isNull(recipe) || StringUtils.isBlank(recipe.toString())){
+                String msg = "Recipe value missing on the samples";
+                clientCallback.displayError(msg);
+                logError(msg);
+                return new PluginResult(false);
+            }
+
+            Object sampleType = attachedSamples.get(0).getValue(SampleModel.EXEMPLAR_SAMPLE_TYPE, user);
+            if (Objects.isNull(sampleType) || StringUtils.isBlank(sampleType.toString())){
+                String msg = "SampleType value missing on the samples";
+                clientCallback.displayError(msg);
+                logError(msg);
+                return new PluginResult(false);
+            }
+
+            // If user submitted pooled libraries, do not run the plugin.
+            if (sampleType.toString().toLowerCase().equals("pooled library")){
+                logInfo("Skipping coverage to reads conversion for user submitted pools");
+                return new PluginResult(false);
+            }
 
             if (seqRequirements.isEmpty()) {
                 this.clientCallback.displayError("Sample 'SequencingRequirements' not attached to this task.");
@@ -63,9 +91,9 @@ public class CoverageToReadsConverter extends DefaultGenericPlugin {
         } catch (NotFound | ServerException | IoError | InvalidValue | RemoteException var6) {
             this.logError(String.valueOf(var6.getStackTrace()));
         }
-
         return new PluginResult(true);
     }
+
 
     /**
      * Method to get SampleIds.
@@ -82,6 +110,7 @@ public class CoverageToReadsConverter extends DefaultGenericPlugin {
             }
         }).collect(Collectors.toList());
     }
+
 
     /**
      * Method to get banked samples related to samples attatched to the task.
@@ -100,6 +129,7 @@ public class CoverageToReadsConverter extends DefaultGenericPlugin {
         return this.dataRecordManager.queryDataRecords("BankedSample", whereClause, this.user);
     }
 
+
     /**
      * Method to get all Recipes that require Coverage.
      * @param coverageReqRefs
@@ -108,29 +138,52 @@ public class CoverageToReadsConverter extends DefaultGenericPlugin {
      * @throws RemoteException
      */
     private List<String> getCoverageRecipes(List<DataRecord> coverageReqRefs) throws NotFound, RemoteException {
-        List<String> recipes = new ArrayList();
+        HashSet<String> recipes = new HashSet<>();
         Iterator references = coverageReqRefs.iterator();
 
         while(references.hasNext()) {
             DataRecord r = (DataRecord)references.next();
-            Object recipe = r.getValue("PlatformApplication", this.user);
+            Object rRecipe = r.getValue("PlatformApplication", this.user);
             Boolean isRefOnly = r.getBooleanVal("ReferenceOnly", this.user);
-            if (!Objects.isNull(recipe) && !isRefOnly) {
-                recipes.add(recipe.toString());
+            if (!Objects.isNull(rRecipe) && !isRefOnly) {
+                recipes.add(rRecipe.toString());
             }
         }
-        return recipes;
+        return new ArrayList(recipes);
     }
+
 
     /**
      * Method to check if a Recipe requires Sequencing Coverage.
-     * @param recipe
      * @param coverageRecipes
      * @return
      */
-    private boolean isCoverageBasedApplication(String recipe, List<String> coverageRecipes) {
+    private boolean isCoverageBasedApplication(List<String> coverageRecipes) {
         return coverageRecipes.contains(recipe);
     }
+
+
+    /**
+     * Method to get the runTypes for a Recipe from the 'ApplicationReadCoverageRef' table in LIMS.
+     * @param refs
+     * @return
+     * @throws NotFound
+     * @throws RemoteException
+     */
+    private List<String> getRunTypeValues(List<DataRecord> refs) throws NotFound, RemoteException {
+        HashSet<String> runTypes = new HashSet<>();
+        Iterator references = refs.iterator();
+        while(references.hasNext()) {
+            DataRecord d = (DataRecord)references.next();
+            Object dRecipe = d.getValue("PlatformApplication", this.user);
+            Object dRunType = d.getValue("SequencingRunType", this.user);
+            if (Objects.equals(recipe, dRecipe) && Objects.nonNull(dRunType)) {
+                runTypes.add((String)dRunType);
+            }
+        }
+        return new ArrayList(runTypes);
+    }
+
 
     /**
      * Method to get RunType
@@ -140,16 +193,19 @@ public class CoverageToReadsConverter extends DefaultGenericPlugin {
      * @throws RemoteException
      * @throws ServerException
      */
-    private Object getRunType(List<DataRecord> bankedSamples) throws NotFound, RemoteException, ServerException {
+    private Object getRunType(List<DataRecord> bankedSamples, List<DataRecord> refs) throws NotFound, RemoteException, ServerException {
         Object runTypeVal = null;
         Iterator banked = bankedSamples.iterator();
-
         do {
             if (!banked.hasNext()) {
                 this.logInfo("Run type after checking on banked samples: " + runTypeVal);
                 if (Objects.isNull(runTypeVal) || StringUtils.isBlank((String)runTypeVal)) {
                     this.logInfo("Prompting user for run type.");
-                    List<String> sequencingRunTypes = this.dataMgmtServer.getPickListManager(this.user).getPickListConfig("Illumina Sequencing Run Types").getEntryList();
+                    List<String> sequencingRunTypes = this.getRunTypeValues(refs);
+                    // if only one RunType value for Recipe, return the RunType and skip the user prompt.
+                    if (sequencingRunTypes.size()==1){
+                        return sequencingRunTypes.get(0);
+                    }
                     List runTypes = this.clientCallback.showListDialog("Select Sequencing Run Type", sequencingRunTypes, false, this.user);
                     this.logInfo("user selected run type: " + runTypes.toString());
                     if (runTypes.size() > 0) {
@@ -168,6 +224,70 @@ public class CoverageToReadsConverter extends DefaultGenericPlugin {
         return runTypeVal;
     }
 
+
+    /**
+     * Method to get PanelNames from 'ApplicationReadCoverageRef' table in LIMS.
+     * @param referenceValues
+     * @return
+     * @throws NotFound
+     * @throws RemoteException
+     */
+    private List<String> getCapturePanelValues(List<DataRecord> referenceValues) throws NotFound, RemoteException {
+        HashSet<String> panels = new HashSet();
+        Iterator references = referenceValues.iterator();
+        while(references.hasNext()) {
+            DataRecord d = (DataRecord)references.next();
+            Object dRecipe = d.getValue("PlatformApplication", this.user);
+            Object dPanel = d.getValue("CapturePanel", this.user);
+            if (Objects.equals(recipe, dRecipe) && Objects.nonNull(dPanel)) {
+                panels.add((String)dPanel);
+            }
+        }
+        return new ArrayList(panels);
+    }
+
+
+    /**
+     * Method to get PanelName value from user
+     * @param referenceValues
+     * @return
+     * @throws NotFound
+     * @throws RemoteException
+     * @throws ServerException
+     */
+    private Object getPanelName(List<DataRecord> referenceValues) throws NotFound, RemoteException, ServerException {
+        Iterator references = referenceValues.iterator();
+        while(true) {
+            Object dRecipe;
+            Object dPanel;
+            do {
+                if (!references.hasNext()) {
+                    return "";
+                }
+                DataRecord d = (DataRecord)references.next();
+                dRecipe = d.getValue("PlatformApplication", this.user);
+                dPanel = d.getValue("CapturePanel", this.user);
+            } while(!Objects.isNull(this.panelName) && !StringUtils.isBlank(this.panelName.toString().trim()));
+
+            if (Objects.nonNull(dPanel) && !StringUtils.isBlank(dPanel.toString()) && Objects.equals(recipe, dRecipe)) {
+                List<String> capturePanels = this.getCapturePanelValues(referenceValues);
+                // if there is only one capture panel for recipe then return the capture panel and skip user prompt.
+                if (capturePanels.size()==1){
+                    return capturePanels.get(0);
+                }
+                String msg = String.format("Recipe '%s' should have a 'CapturePanel' value. Missing 'CapturePanel' value may result in failure to update RequestedReads/RequestedCoverage. Select 'OK' to pick CapturePanel value.", recipe);
+                boolean selectPanel = this.clientCallback.showYesNoDialog(String.format("Missing 'Capture Panel' value for Recipe '%s'.", recipe), msg);
+                if (selectPanel) {
+                    List panelSelected = this.clientCallback.showListDialog("Select CapturePanel/Baitset from the list", capturePanels, false, this.user);
+                    if (panelSelected.size() > 0) {
+                        return panelSelected.get(0);
+                    }
+                }
+            }
+        }
+    }
+
+
     /**
      * Method to update SequencingRequirements from 'ApplicationReadCoverageRef' values.
      * @param samples
@@ -184,12 +304,10 @@ public class CoverageToReadsConverter extends DefaultGenericPlugin {
     private void updateSeqReq(List<DataRecord> samples, List<DataRecord> bankedSamples, List<String> coverageRecipes, List<DataRecord> seqRequirements, List<DataRecord> coverageReqRefs) throws NotFound, RemoteException, ServerException, IoError, InvalidValue {
         this.logInfo(bankedSamples.toString());
         if (Objects.isNull(this.runType) || this.runType == "") {
-            this.runType = this.getRunType(bankedSamples);
+            this.runType = this.getRunType(bankedSamples, coverageReqRefs);
         }
-
         this.logInfo("Run Type after prompt : " + this.runType);
         Iterator sampleIter = samples.iterator();
-
         while(true) {
             while(true) {
                 while(sampleIter.hasNext()) {
@@ -197,13 +315,12 @@ public class CoverageToReadsConverter extends DefaultGenericPlugin {
                     Object igoId = s.getValue("SampleId", this.user);
                     Object sampleId = s.getValue("OtherSampleId", this.user);
                     Object altId = s.getValue("AltId", this.user);
-                    Object recipe = s.getValue("Recipe", this.user);
                     this.logInfo("Recipe: " + recipe);
                     Object species = s.getValue("Species", this.user);
                     Object tumorOrNormal = s.getValue("TumorOrNormal", this.user);
                     Object reads = null;
                     Object coverage = null;
-                    boolean isCoverageRecipe = recipe != null && this.isCoverageBasedApplication((String)recipe, coverageRecipes);
+                    boolean isCoverageRecipe = this.isCoverageBasedApplication(coverageRecipes);
                     this.logInfo("Is coverage Recipe: " + isCoverageRecipe);
                     this.logInfo("Sample ID: " + sampleId);
                     Iterator banked = bankedSamples.iterator();
@@ -234,7 +351,7 @@ public class CoverageToReadsConverter extends DefaultGenericPlugin {
                     }
                     if (isCoverageRecipe) {
                         if (!this.panelSelectionOffered && (Objects.isNull(this.panelName) || StringUtils.isBlank(this.panelName.toString().trim()))) {
-                            this.panelName = this.getPanelName(recipe, coverageReqRefs);
+                            this.panelName = this.getPanelName(coverageReqRefs);
                             this.panelSelectionOffered = true;
                         }
                         reads = CoverageToReadsUtil.getCoverageReadsFromRefs(recipe, this.panelName, species, this.runType, tumorOrNormal, coverage, coverageReqRefs, this.user, this.pluginLogger);
@@ -278,64 +395,5 @@ public class CoverageToReadsConverter extends DefaultGenericPlugin {
                 return;
             }
         }
-    }
-
-    /**
-     * Method to get PanelName value from user
-     * @param recipe
-     * @param referenceValues
-     * @return
-     * @throws NotFound
-     * @throws RemoteException
-     * @throws ServerException
-     */
-    private Object getPanelName(Object recipe, List<DataRecord> referenceValues) throws NotFound, RemoteException, ServerException {
-        Iterator references = referenceValues.iterator();
-        while(true) {
-            Object dRecipe;
-            Object dPanel;
-            do {
-                if (!references.hasNext()) {
-                    return "";
-                }
-                DataRecord d = (DataRecord)references.next();
-                dRecipe = d.getValue("PlatformApplication", this.user);
-                dPanel = d.getValue("CapturePanel", this.user);
-            } while(!Objects.isNull(this.panelName) && !StringUtils.isBlank(this.panelName.toString().trim()));
-
-            if (Objects.nonNull(dPanel) && !StringUtils.isBlank(dPanel.toString()) && Objects.equals(recipe, dRecipe)) {
-                List<String> capturePanels = this.getCapturePanelValues(recipe, referenceValues);
-                String msg = String.format("Recipe '%s' should have a 'CapturePanel' value. Missing 'CapturePanel' value may result in failure to update RequestedReads/RequestedCoverage. Select 'OK' to pick CapturePanel value.", recipe);
-                boolean selectPanel = this.clientCallback.showYesNoDialog(String.format("Missing 'Capture Panel' value for Recipe '%s'.", recipe), msg);
-                if (selectPanel) {
-                    List panelSelected = this.clientCallback.showListDialog("Select CapturePanel/Baitset from the list", capturePanels, false, this.user);
-                    if (panelSelected.size() > 0) {
-                        return panelSelected.get(0);
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * Method to get PanelNames from 'ApplicationReadCoverageRef' table in LIMS.
-     * @param recipe
-     * @param referenceValues
-     * @return
-     * @throws NotFound
-     * @throws RemoteException
-     */
-    private List<String> getCapturePanelValues(Object recipe, List<DataRecord> referenceValues) throws NotFound, RemoteException {
-        HashSet<String> panels = new HashSet();
-        Iterator references = referenceValues.iterator();
-        while(references.hasNext()) {
-            DataRecord d = (DataRecord)references.next();
-            Object dRecipe = d.getValue("PlatformApplication", this.user);
-            Object dPanel = d.getValue("CapturePanel", this.user);
-            if (Objects.equals(recipe, dRecipe) && Objects.nonNull(dPanel)) {
-                panels.add((String)dPanel);
-            }
-        }
-        return new ArrayList(panels);
     }
 }
