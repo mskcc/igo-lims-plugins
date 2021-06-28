f7484794e83108cbf093dfc31cefda246cbaa1c2
Mirhaj <f.mirhaj@gmail.com>
2021-06-28 11:16:27 -0400

    Adding test cases to unit test.

--- src/test/java/com/velox/sloan/cmo/workflows/samplereceiving/sequencingrequirements/SequencingRequirementsHandlerTest.java
+++ src/test/java/com/velox/sloan/cmo/workflows/samplereceiving/sequencingrequirements/SequencingRequirementsHandlerTest.java
@@ -21,18 +21,21 @@ public class SequencingRequirementsHandlerTest extends SequencingRequirementsHan
             List<DataRecord> attachedSamples = this.activeTask.getAttachedDataRecords("Sample", this.user);
             List<DataRecord> seqRequirements = this.activeTask.getAttachedDataRecords("SeqRequirement", this.user);
             //ImmunoSeq
-            if (attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("")) {
+            if (attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("07340_B_53_2_1_1_1")) {
                 assertEquals(seqRequirements.get(0).getValue("SequencingRunType", this.user).toString(), "");
             }
             //ddPCR, DLP, CellLineAuthentication, COVID19
-            if (attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("")) {
+            if (attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("05816_CM_1_1")
+                    || attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("09875_1")
+                    || attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("05134_H_1")
+                    || attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("06000_FZ_1")) {
                 assertEquals(pr, new PluginResult(true));
             }
 
             // requested reads empty
             // coverage is empty for the recipe
-            if (attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("")) {
-                assertEquals(seqRequirements.get(0).getValue("MaxReads", this.user).toString(), "");
+            if (attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("UPS_PBMC_02")) {
+                assertEquals(seqRequirements.get(0).getValue("MaxReads", this.user).toString(), "10.0");
             }
 
             //banked sample coverage is empty & sequencing requirements requested reads is empty
@@ -40,8 +43,9 @@ public class SequencingRequirementsHandlerTest extends SequencingRequirementsHan
 
             //EXPECTED: ask LIMS user to select capture panel
             // check max reads & coverage values
-            if (attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("")) {
+            if (attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("07555_1")) {
                 assertEquals(seqRequirements.get(0).getValue("MaxReads", this.user).toString(), "");
+                assertEquals(seqRequirements.get(0).getValue("Coverage", this.user).toString(), "");
             }
 
             // if banked sample capture panel has a value
