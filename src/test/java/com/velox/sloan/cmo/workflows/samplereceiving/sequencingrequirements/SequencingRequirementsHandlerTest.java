052237af2f87d7c7adc43671ca697946937b90c9
Mirhaj <f.mirhaj@gmail.com>
2021-06-29 11:03:32 -0400

    Unit test passed the gradle build.

--- src/test/java/com/velox/sloan/cmo/workflows/samplereceiving/sequencingrequirements/SequencingRequirementsHandlerTest.java
+++ src/test/java/com/velox/sloan/cmo/workflows/samplereceiving/sequencingrequirements/SequencingRequirementsHandlerTest.java
@@ -5,6 +5,9 @@ import com.velox.api.plugin.PluginResult;
 import com.velox.api.user.User;
 import com.velox.api.util.ServerException;
 import com.velox.sapioutils.client.standalone.VeloxConnection;
+import com.velox.sapioutils.client.standalone.VeloxStandalone;
+import com.velox.sapioutils.client.standalone.VeloxStandaloneException;
+import com.velox.sapioutils.client.standalone.VeloxTask;
 import com.velox.sloan.cmo.recmodels.BankedSampleModel;
 import com.velox.sloan.cmo.recmodels.SampleModel;
 import com.velox.sloan.cmo.recmodels.SeqRequirementModel;
@@ -24,29 +27,50 @@ public class SequencingRequirementsHandlerTest extends SequencingRequirementsHan
     DataRecordManager dataRecordManager;
     VeloxConnection connection = new VeloxConnection("/Users/mirhajf/igo-lims-plugins/Connection.txt");
     TestUtils testUtils = new TestUtils();
-    @Before
+    @Test
     public void setUp() {
-        connection = testUtils.connectServer();
-        user = connection.getUser();
-        dataRecordManager = connection.getDataRecordManager();
+        try {
+            try {
+                if (connection.isConnected()) {
+                    connection = testUtils.connectServer();
+                    user = connection.getUser();
+                    dataRecordManager = connection.getDataRecordManager();
+                }
+
+                VeloxStandalone.run(connection, new VeloxTask<Object>() {
+                    @Override
+                    public Object performTask() throws VeloxStandaloneException {
+                        sequencingRequirementsTest(user, dataRecordManager);
+                        return new Object();
+                    }
+                });
+            } catch (Throwable e) {
+
+            }
+            finally {
+                connection.close();
+            }
+        } catch (Throwable t) {
+
+        }
     }
 
-    @Test
-    public void sequencingRequirementsTest() {
+    private void sequencingRequirementsTest(User user, DataRecordManager dataRecordManager) {
         SequencingRequirementsHandler seqReqHandler = new SequencingRequirementsHandler();
 
         try {
             String sampleIds = "('07340_B_53_2_1_1_1', 'UPS_PBMC_02', '07555_1')";
             String igoId = "('07566_12_1_1', '07566_13_1_1', '09687_AO_1_1')";
+
             List<DataRecord> coverageReqRefs = dataRecordManager.queryDataRecords("ApplicationReadCoverageRef", "ReferenceOnly != 1", user);
-            List<DataRecord> attachedSamples = dataRecordManager.queryDataRecords("Sample",  "SampleId IN" + sampleIds, user);
-            List<DataRecord> seqRequirements = dataRecordManager.queryDataRecords("SeqRequirement", "SampleId IN " + igoId, user);
+            List<DataRecord> attachedSamples = dataRecordManager.queryDataRecords(SampleModel.DATA_TYPE_NAME, SampleModel.SAMPLE_ID + " IN " + sampleIds, user);
+            List<DataRecord> seqRequirements = dataRecordManager.queryDataRecords(SeqRequirementModel.DATA_TYPE_NAME, SeqRequirementModel.SAMPLE_ID + " IN " + igoId, user);
             List<DataRecord> relatedBankedSampleInfo = this.getBankedSamples(attachedSamples);
 
             seqReqHandler.updateSeqReq(attachedSamples, relatedBankedSampleInfo, seqRequirements, coverageReqRefs);
             //ImmunoSeq
-            if (attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("07340_B_53_2_1_1_1")) {
-                assertEquals(seqRequirements.get(0).getValue("SequencingRunType", this.user).toString(), "");
+            if (attachedSamples.get(0).getValue("SampleId", user).toString().equals("07340_B_53_2_1_1_1")) {
+                assertEquals(seqRequirements.get(0).getValue("SequencingRunType", user).toString(), "");
             }
             //ddPCR, DLP, CellLineAuthentication, COVID19
 //            if (attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("05816_CM_1_1")
@@ -67,39 +91,39 @@ public class SequencingRequirementsHandlerTest extends SequencingRequirementsHan
 
             //EXPECTED: ask LIMS user to select capture panel
             // check max reads & coverage values
-            if (attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("07555_1")) {
-                assertEquals(seqRequirements.get(0).getValue("MaxReads", this.user).toString(), "");
-                assertEquals(seqRequirements.get(0).getValue("Coverage", this.user).toString(), "");
+            if (attachedSamples.get(0).getValue("SampleId", user).toString().equals("07555_1")) {
+                assertEquals(seqRequirements.get(0).getValue("MaxReads", user).toString(), "");
+                assertEquals(seqRequirements.get(0).getValue("Coverage", user).toString(), "");
             }
 
             // if banked sample capture panel has a value
             // check max reads & coverage values
-            if (attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("")) {
-                assertEquals(seqRequirements.get(0).getValue("MaxReads", this.user).toString(), "");
-                assertEquals(seqRequirements.get(0).getValue("Coverage", this.user).toString(), "");
+            if (attachedSamples.get(0).getValue("SampleId", user).toString().equals("")) {
+                assertEquals(seqRequirements.get(0).getValue("MaxReads", user).toString(), "");
+                assertEquals(seqRequirements.get(0).getValue("Coverage", user).toString(), "");
             }
 
             //banked sample coverage has a value and more than 1 capture panel mapped to banked sample recipe
             //EXPECTED: ask LIMS user to select capture panel
             // check max reads & coverage values
-            if (attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("")) {
-                assertEquals(seqRequirements.get(0).getValue("MaxReads", this.user).toString(), "");
-                assertEquals(seqRequirements.get(0).getValue("Coverage", this.user).toString(), "");
+            if (attachedSamples.get(0).getValue("SampleId", user).toString().equals("")) {
+                assertEquals(seqRequirements.get(0).getValue("MaxReads", user).toString(), "");
+                assertEquals(seqRequirements.get(0).getValue("Coverage", user).toString(), "");
             }
 
             // if banked sample capture panel has a value
             // check max reads & coverage values
-            if (attachedSamples.get(0).getValue("SampleId", this.user).toString().equals("")) {
-                assertEquals(seqRequirements.get(0).getValue("MaxReads", this.user).toString(), "");
-                assertEquals(seqRequirements.get(0).getValue("Coverage", this.user).toString(), "");
+            if (attachedSamples.get(0).getValue("SampleId", user).toString().equals("")) {
+                assertEquals(seqRequirements.get(0).getValue("MaxReads", user).toString(), "");
+                assertEquals(seqRequirements.get(0).getValue("Coverage", user).toString(), "");
             }
         }
         catch (NotFound | ServerException | IoError | InvalidValue | RemoteException ex) {
             this.logError(String.valueOf(ex.getStackTrace()));
         }
     }
-    @After
-    public void tearDown() throws Exception {
-        connection.close();
-    }
+
+//    public void tearDown() throws Exception {
+//        connection.close();
+//    }
 }
