6d76149b17f87928bcb05b7ad12afa609498484a
Mirhaj <f.mirhaj@gmail.com>
2023-06-21 10:29:01 -0400

    Updating sample type at sample and dna extraction experiment levels for ONT

--- src/main/java/com/velox/sloan/cmo/workflows/workflows/DNAExtraction/ChangeSampleTypeTohmwDNA.java
+++ src/main/java/com/velox/sloan/cmo/workflows/workflows/DNAExtraction/ChangeSampleTypeTohmwDNA.java
@@ -1,36 +1,54 @@
 package com.velox.sloan.cmo.workflows.workflows.DNAExtraction;
 
 import com.velox.api.datarecord.DataRecord;
+import com.velox.api.datarecord.InvalidValue;
+import com.velox.api.datarecord.IoError;
 import com.velox.api.datarecord.NotFound;
 import com.velox.api.plugin.PluginResult;
 import com.velox.sapioutils.server.plugin.DefaultGenericPlugin;
+import com.velox.sapioutils.shared.enums.PluginOrder;
 
 
 import java.rmi.RemoteException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ChangeSampleTypeTohmwDNA extends DefaultGenericPlugin {
 
+    public ChangeSampleTypeTohmwDNA() {
+        setTaskEntry(true);
+        setOrder(PluginOrder.LAST.getOrder());
+    }
+
     @Override
     protected boolean shouldRun() throws Throwable {
-        return this.activeTask.getTask().getTaskName().equalsIgnoreCase("create experiment") &&
+        return this.activeTask.getTask().getTaskName().equalsIgnoreCase("DNA Extraction") &&
                 this.activeTask.getTask().getTaskOptions().containsKey("CHANGE SAMPLE TYPE TO HMWDNA");
     }
     public PluginResult run() {
         try {
             List<DataRecord> samples = activeTask.getAttachedDataRecords("Sample", user);
-            Boolean isONT= activeTask.getAttachedDataRecords("DNAExperiment", user).get(0).getBooleanVal("IsONT", user);
-            Map<String, Object> sampleType = new HashMap<>();
-            if (isONT) {
+            List<DataRecord> dnaExtractionExpriments = activeTask.getAttachedDataRecords("DNAExtractionExperiment", user);
+            int i = 0;
                 for (DataRecord sample : samples) {
-                    sampleType.put("SampleType", "hmwDNA");
-                    sample.setFields(sampleType, user);
-                }
-            }
+                    Boolean isONT= dnaExtractionExpriments.get(i++).getBooleanVal("IsONT", user);
+                    if (isONT) {
+                        logInfo("isONT = true, changing sample type");
+                        sample.setDataField("ExemplarSampleType", "hmwDNA", user);
+                    }
 
-        } catch (NotFound | com.velox.api.util.ServerException | RemoteException e) {
+                }
+                i = 0;
+                for (DataRecord dnaExperiment : dnaExtractionExpriments) {
+                    Boolean isONT= dnaExtractionExpriments.get(i++).getBooleanVal("IsONT", user);
+                    if (isONT) {
+                        logInfo("isONT = true, changing extraction sample type");
+                        dnaExperiment.setDataField("ExtractionSampleType", "hmwDNA", user);
+                    }
+                }
+
+
+        } catch (InvalidValue | IoError |NotFound | com.velox.api.util.ServerException | RemoteException e) {
+            logError("An exception occurred while setting sample type to hmwDNA", e);
 
         }
         return new PluginResult(true);
