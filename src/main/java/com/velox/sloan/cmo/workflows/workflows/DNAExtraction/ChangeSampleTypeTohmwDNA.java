041954744c81ecec0f2c26305c1bcd5b81200793
Mirhaj <f.mirhaj@gmail.com>
2023-06-27 13:33:33 -0400

    Updating ONT sample types to hmwDNA.

--- src/main/java/com/velox/sloan/cmo/workflows/workflows/DNAExtraction/ChangeSampleTypeTohmwDNA.java
+++ src/main/java/com/velox/sloan/cmo/workflows/workflows/DNAExtraction/ChangeSampleTypeTohmwDNA.java
@@ -9,13 +9,14 @@ import com.velox.sapioutils.server.plugin.DefaultGenericPlugin;
 import com.velox.sapioutils.shared.enums.PluginOrder;
 
 
+import javax.xml.crypto.Data;
 import java.rmi.RemoteException;
 import java.util.List;
 
 public class ChangeSampleTypeTohmwDNA extends DefaultGenericPlugin {
 
     public ChangeSampleTypeTohmwDNA() {
-        setTaskEntry(true);
+        setTaskSubmit(true);
         setOrder(PluginOrder.LAST.getOrder());
     }
 
@@ -28,25 +29,19 @@ public class ChangeSampleTypeTohmwDNA extends DefaultGenericPlugin {
         try {
             List<DataRecord> samples = activeTask.getAttachedDataRecords("Sample", user);
             List<DataRecord> dnaExtractionExpriments = activeTask.getAttachedDataRecords("DNAExtractionExperiment", user);
-            int i = 0;
-                for (DataRecord sample : samples) {
-                    Boolean isONT= dnaExtractionExpriments.get(i++).getBooleanVal("IsONT", user);
-                    if (isONT) {
-                        logInfo("isONT = true, changing sample type");
-                        sample.setDataField("ExemplarSampleType", "hmwDNA", user);
-                    }
+            logInfo("dnaExtractionExpriments size = " + dnaExtractionExpriments.size());
+            // always one experiment level record is created for all samples on the current workflow
+            Boolean isONT = dnaExtractionExpriments.get(0).getBooleanVal("IsONT", user);
+            logInfo("isONT = " + isONT);
+            if (isONT) {
+                logInfo("isONT = true, changing extraction sample type");
+                dnaExtractionExpriments.get(0).setDataField("ExtractionSampleType", "hmwDNA", user);
+            }
 
-                }
-                i = 0;
-                for (DataRecord dnaExperiment : dnaExtractionExpriments) {
-                    Boolean isONT= dnaExtractionExpriments.get(i++).getBooleanVal("IsONT", user);
-                    if (isONT) {
-                        logInfo("isONT = true, changing extraction sample type");
-                        dnaExperiment.setDataField("ExtractionSampleType", "hmwDNA", user);
-                    }
-                }
-
-
+            for (DataRecord sample : samples) {
+                logInfo("isONT = true, changing sample type");
+                sample.setDataField("ExemplarSampleType", "hmwDNA", user);
+            }
         } catch (InvalidValue | IoError |NotFound | com.velox.api.util.ServerException | RemoteException e) {
             logError("An exception occurred while setting sample type to hmwDNA", e);
 
