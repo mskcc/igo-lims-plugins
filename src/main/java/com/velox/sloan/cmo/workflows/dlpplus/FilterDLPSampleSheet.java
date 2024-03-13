3f75e2ecb2a760ed8b2a7d17aab36360d532f341
Mirhaj <mirhajf@lski2475.mskcc.org>
2024-03-13 15:57:29 -0400

    test

--- src/main/java/com/velox/sloan/cmo/workflows/dlpplus/FilterDLPSampleSheet.java
+++ src/main/java/com/velox/sloan/cmo/workflows/dlpplus/FilterDLPSampleSheet.java
@@ -7,7 +7,7 @@ public class FilterDLPSampleSheet {
 
 @author mirhajf
 */
-    private File filterDLPSampleSheet(File inputSampleSheet, File fld) {
+    private void filterDLPSampleSheet(File inputSampleSheet, File fld) {
         /*
          * 5th field in the sample sheet contains the chip location
          * Lane,Sample_ID,Sample_Plate,Sample_Well,I7_Index_ID,index,index2,Sample_Project,Description
@@ -15,26 +15,15 @@ public class FilterDLPSampleSheet {
          * DLPi7_01-i5_01: split by "_", take indexes 1 (split by "-" and take index 0) and 3
          * on the fld file find 1/1. On the sample sheet only copy the lines with chip locations that has a value on its corresponding
          **/
-//        String filePath = "example.fld";
-
         try {
-            // make a copy of the original sample sheet
-            FileInputStream inputStream = new FileInputStream(inputSampleSheet);
-            FileOutputStream outputStream = new FileOutputStream("DLPSampleSheetCopied.csv");
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-            System.out.println("File copied successfully!");
-
             BufferedReader sampleSheetReader = new BufferedReader(new FileReader(inputSampleSheet));
             BufferedReader fieldFileReader = new BufferedReader(new FileReader(fld));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("filteredDLPSampleSheet.csv"));
             String fieldLine, sampleSheetLine;
             // Skipping the field and the sample sheet header lines
-            int sampleSheetHeaderLinesToSkip = 18;
-            for (int i = 0; i < sampleSheetHeaderLinesToSkip; i++) {
-                sampleSheetReader.readLine();
+            int sampleSheetHeaderLinesToCopy = 18;
+            for (int i = 0; i < sampleSheetHeaderLinesToCopy; i++) {
+                writer.write(sampleSheetReader.readLine());
             }
             int fieldFileHeaderLinesToSkip = 24;
             for (int i = 0; i < fieldFileHeaderLinesToSkip; i++) {
@@ -46,22 +35,17 @@ public class FilterDLPSampleSheet {
                 String fieldRow = fieldLine.split("/")[0];
                 String fieldCol = fieldLine.split("/")[1];
                 if (fieldLine.split(",").length > 0) {
-
-                }
-                while ((sampleSheetLine = sampleSheetReader.readLine()) != null) {
-                    String[] sampleFields = sampleSheetLine.split(",");
-                    String fieldWithChipLocInfo = sampleFields[4];
-                    String row = fieldWithChipLocInfo.split("_")[1].split("-")[0];
-                    String col = fieldWithChipLocInfo.split("_")[2];
-                    if (fieldRow.equals(row) && fieldCol.equals(col)) {
-                        // keep this line
+                    while ((sampleSheetLine = sampleSheetReader.readLine()) != null) {
+                        String[] sampleFields = sampleSheetLine.split(",");
+                        String fieldWithChipLocInfo = sampleFields[4];
+                        String row = fieldWithChipLocInfo.split("_")[1].split("-")[0];
+                        String col = fieldWithChipLocInfo.split("_")[2];
+                        if (fieldRow.equals(row) && fieldCol.equals(col)) {
+                            // keep this line
+                            writer.write(sampleSheetLine);
+                        }
                     }
-
                 }
-                if (fieldLine.startsWith("1/1")) {
-
-                }
-
             }
         } catch (IOException e) {
             e.printStackTrace();
