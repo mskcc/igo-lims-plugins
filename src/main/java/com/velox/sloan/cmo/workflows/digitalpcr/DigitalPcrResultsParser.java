961e977df435f3c7186eb7d90386c28f3b0b70f6
Mirhaj <mirhajf@lski2475.mskcc.org>
2024-07-29 16:35:55 -0400

    Correcting human percentage calculation based on gen adn ref copies

--- src/main/java/com/velox/sloan/cmo/workflows/digitalpcr/DigitalPcrResultsParser.java
+++ src/main/java/com/velox/sloan/cmo/workflows/digitalpcr/DigitalPcrResultsParser.java
@@ -343,7 +343,7 @@ public class DigitalPcrResultsParser extends DefaultGenericPlugin {
                 analyzedData.put("Ratio", ratio);
                 analyzedData.put("AcceptedDroplets", getSum(groupedData.get(key), "AcceptedDroplets"));
                 if (target.equalsIgnoreCase(HUMAN_MOUSE_PERCENTAGE_ASSAY_NAME)) {
-                    Double humanPercentage = calculateHumanPercentage(dropletCountMutation, dropletCountWildType);
+                    Double humanPercentage = calculateHumanPercentage((Double) analyzedData.get("ConcentrationMutation"), (Double) analyzedData.get("ConcentrationWildType"));
                     analyzedData.put("HumanPercentage", humanPercentage);
                 }
                 analyzedData.put("TotalInput", getTotalInputForSample(sampleName, target, protocolRecords));
