4504f86cc36518dca7e1ef75cea4d392553c5751
Mirhaj <mirhajf@lski2475.mskcc.org>
2024-10-02 16:21:35 -0400

    Pushing Xenium updates

--- src/main/java/com/velox/sloan/cmo/workflows/workflows/SpatialTranscriptomics/DiactivateWorkflowStepsForXenium.java
+++ src/main/java/com/velox/sloan/cmo/workflows/workflows/SpatialTranscriptomics/DiactivateWorkflowStepsForXenium.java
@@ -1,5 +1,12 @@
 package com.velox.sloan.cmo.workflows.workflows.SpatialTranscriptomics;
 
+import com.velox.api.datarecord.DataRecord;
+import com.velox.api.plugin.PluginResult;
+import com.velox.sapio.commons.exemplar.plugin.PluginOrder;
+import com.velox.sapioutils.server.plugin.DefaultGenericPlugin;
+
+import java.util.List;
+
 public class DiactivateWorkflowStepsForXenium extends DefaultGenericPlugin {
     public DiactivateWorkflowStepsForXenium() {
         setTaskSubmit(true);
@@ -8,14 +15,23 @@ public class DiactivateWorkflowStepsForXenium extends DefaultGenericPlugin {
 
     @Override
     protected boolean shouldRun() throws Throwable {
-        return this.activeTask.getTask().getTaskName().equalsIgnoreCase("") &&
-                this.activeTask.getTask().getTaskOptions().containsKey("");
+        return this.activeTask.getTask().getTaskName().equalsIgnoreCase("Record Permeabilization Time and Fluorescence") &&
+                this.activeTask.getTask().getTaskOptions().containsKey("Deactivate through six if Xenium");
     }
     public PluginResult run() {
         try {
-
+            List<DataRecord> attachedSamples = activeTask.getAttachedDataRecords("Sample", user);
+            List<DataRecord> attachedcDNAProtocols = activeTask.getAttachedDataRecords("VisiumcDNAPrepProtocol1", user);
+            if (attachedcDNAProtocols.get(0).getBooleanVal("Xenium", user)) {
+                logInfo("Disabling steps 4 through 6");
+                logInfo("Active task ID = " + this.activeTask.getTask().getTaskId());
+                this.activeWorkflow.getActiveTask(1143772L).setStatus(0);
+                this.activeWorkflow.getActiveTask(1143773L).setStatus(0);
+                this.activeWorkflow.getActiveTask(1143774L).setStatus(0);
+            }
         } catch (Exception e) {
-
+            logError("Exception occurred on step 3 of Spatial Transcriptomics workflow: " + e.getMessage() + "\n" +
+                    e.getStackTrace());
         }
         return new PluginResult(true);
     }
