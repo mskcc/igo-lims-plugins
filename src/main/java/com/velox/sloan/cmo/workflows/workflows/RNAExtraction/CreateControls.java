2351bd5c7d0937c58fb05e0c699d14381bced8b5
Fahimeh Mirhaj <f.mirhaj@gmail.com>
2025-07-29 11:42:27 -0400

    Assigning igo id

--- src/main/java/com/velox/sloan/cmo/workflows/workflows/RNAExtraction/CreateControls.java
+++ src/main/java/com/velox/sloan/cmo/workflows/workflows/RNAExtraction/CreateControls.java
@@ -16,17 +16,20 @@ public class CreateControls extends DefaultGenericPlugin {
     }
     @Override
     public boolean shouldRun() throws RemoteException {
-        return activeTask.getStatus() != activeTask.COMPLETE && activeTask.getTask().getTaskOptions().containsKey("Generate Control Records");
+        return activeTask.getStatus() != activeTask.COMPLETE && activeTask.getTask().getTaskOptions().containsKey("Generate Control Records")
+                && !activeTask.getTask().getTaskOptions().containsKey("Control Records Generated");
     }
     public PluginResult run() {
         try {
             List<DataRecord> samples = activeTask.getAttachedDataRecords("Sample", user);
             String sampleType = samples.get(0).getStringVal("ExemplarSampleType", user);
+            String requestId = samples.get(0).getParentsOfType("Request", user).get(0).getStringVal("RequestId", user);
             List<String> controlList = clientCallback.showListDialog("Please Select All the Type of the Control(s) to Generate:", controlTypes, true, user);
             List<DataRecord> allControls = new LinkedList<>();
             for (String control : controlList) {
                 Map<String, Object> values = new HashMap<>();
                 values.put("Volume", 30);
+                values.put("SampleId", control + "_" + requestId);
                 values.put("OtherSampleId", control);
                 values.put("ExemplarSampleType", sampleType);
                 DataRecord controlRec = dataRecordManager.addDataRecord("Sample", user);
@@ -34,6 +37,7 @@ public class CreateControls extends DefaultGenericPlugin {
                 allControls.add(controlRec);
             }
             this.activeTask.addAttachedDataRecords(allControls);
+            activeTask.getTask().getTaskOptions().put("Control Records Generated", "");
         }
         catch (Exception e) {
             return new PluginResult(false);
